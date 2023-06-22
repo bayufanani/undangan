d917beddda0da5e499fa015f7b7a1ddd00c8f3b9
Bayu Fanani <fanani707@gmail.com>
2023-06-22 23:03:46 +0700

    Updates

--- _app/immutable/chunks/stores.01d7b24a.js
+++
@@ -1,1 +0,0 @@
-import"./index.3ed794e4.js";import{s as e}from"./singletons.7d1b4318.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
